aef9fc739795edd1cd3c31f1f069173a5f1f311b
Bernardo Meirelles <bernardomrl@icloud.com>
2024-03-22 13:02:35 -0300

    fix: :coffin: remove unused exports

--- src/types/props/index.ts
+++ src/types/props/index.ts
@@ -1,4 +1,1 @@
-export type { ContextProps } from '@/types/props/context';
 export type { FiltersProps } from '@/types/props/filters';
-export type { LayoutProps } from '@/types/props/layout';
-export type { DrawerProps } from '@/types/props/drawer';
